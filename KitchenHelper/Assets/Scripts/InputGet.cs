9f736af2a109246c469ee4a98299f178c6c135f7
Armani <137173487+LuminousArmani@users.noreply.github.com>
2024-10-23 13:33:44 -0700

    Got 2 calculators working simultaneously

--- KitchenHelper/Assets/Scripts/InputGet.cs
+++ KitchenHelper/Assets/Scripts/InputGet.cs
@@ -6,21 +6,25 @@ using UnityEngine.UI;
 
 public class InputGet : MonoBehaviour
 {
-    
     Button cal;
-    [SerializeField]
-    TMP_InputField field;
     [SerializeField]
     TMP_InputField i_eggamount;
     [SerializeField]
     TMP_InputField i_eggcost;
     [SerializeField]
     TMP_InputField i_eggtotal;
+    [SerializeField]
+    TMP_InputField i_flouramount;
+    [SerializeField]
+    TMP_InputField i_flourcost;
+    [SerializeField]
+    TMP_InputField i_flourtotal;
+    [SerializeField]
+
     // Start is called before the first frame update
     void Start()
     {
-        i_eggcost = GetComponent<TMP_InputField>();
-        i_eggamount = GetComponent<TMP_InputField>();
+
     }
 
     // Update is called once per frame
@@ -33,14 +37,37 @@ public class InputGet : MonoBehaviour
     }
    public void calculate()
     {
-        float eggcost = -1;
-        Debug.Log(i_eggamount.text);
-        Debug.Log(float.TryParse(i_eggcost.text, out eggcost));
-        float eggamount = -1;
-        Debug.Log(float.TryParse(i_eggamount.text, out eggamount));
-        Debug.Log(i_eggtotal.text);
-        Debug.Log(i_eggtotal);
-        float eggtotal = eggamount * eggcost;
-        i_eggtotal.text = eggtotal.ToString();
+        if (i_eggamount && i_eggcost == null)
+        {
+            i_eggtotal.text = string.Empty;
+        }
+        else if (i_eggamount && i_eggcost != null)
+        {
+            float eggcost = -1;
+            Debug.Log(i_eggamount.text);
+            Debug.Log(float.TryParse(i_eggcost.text, out eggcost));
+            float eggamount = -1;
+            Debug.Log(float.TryParse(i_eggamount.text, out eggamount));
+            Debug.Log(i_eggtotal.text);
+            Debug.Log(i_eggtotal);
+            float eggtotal = eggamount * eggcost;
+            i_eggtotal.text = eggtotal.ToString();
+        }
+        if (i_flouramount && i_flourcost == null)
+        {
+            i_flourtotal.text = string.Empty;
+        }
+        else if (i_flourcost && i_flouramount != null)
+        {
+            float flourcost = -1;
+            Debug.Log(i_flouramount.text);
+            Debug.Log(float.TryParse(i_flourcost.text, out flourcost));
+            float flouramount = -1;
+            Debug.Log(float.TryParse(i_flouramount.text, out flouramount));
+            Debug.Log(i_flourtotal.text);
+            Debug.Log(i_flourtotal);
+            float flourtotal = flouramount * flourcost;
+            i_flourtotal.text = flourtotal.ToString();
+        }
     }
 }
---
+++ KitchenHelper/Assets/Scripts/Test scripts/Big Calculator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigCalculator : MonoBehaviour
+{
+
+    List<int> Numbers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Numbers = new List<int>(10);
+        Numbers.Add(00);
+        Numbers.Add(10);
+        Numbers.Add(20);
+        Numbers.Add(30);
+        Numbers.Add(40);
+        Numbers.Add(50);
+        Numbers.Add(60);
+        Numbers.Add(70);
+        Numbers.Add(80);
+        Numbers.Add(90);
+        Numbers.Add(100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+            Debug.Log(Numbers[0]);
+        }
+    }
+}
